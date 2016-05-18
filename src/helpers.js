47e5980b989e82ebde94a7060b0b497f9aef3ceb
Konstantin Grushetsky <toxickg@openmailbox.org>
2016-05-18 03:37:09 +0300

    chore: move "@" character from 'formatTime' into entry group title itself

--- src/helpers.js
+++ src/helpers.js
@@ -2,7 +2,7 @@ const repeat = (str, times) => (new Array(times + 1)).join(str);
 
 const pad = (num, maxLength) => repeat(`0`, maxLength - num.toString().length) + num;
 
-export const formatTime = (time) => `@ ${pad(time.getHours(), 2)}:${pad(time.getMinutes(), 2)}:${pad(time.getSeconds(), 2)}.${pad(time.getMilliseconds(), 3)}`;
+export const formatTime = (time) => `${pad(time.getHours(), 2)}:${pad(time.getMinutes(), 2)}:${pad(time.getSeconds(), 2)}.${pad(time.getMilliseconds(), 3)}`;
 
 // Use performance API if it's available in order to get better precision
 export const timer = typeof performance !== `undefined` && typeof performance.now === `function` ? performance : Date;
