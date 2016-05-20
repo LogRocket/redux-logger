ae8eb0e3739817b17c77b24e25fa98e51bb63c25
Konstantin Grushetsky <toxickg@openmailbox.org>
2016-05-21 02:40:48 +0300

    misc: configure test environment and code coverage
    Add code coverage reporting (console, HTML), prepare Travis CI config file for Codecov integration, update Node.js version used by Travis CI.

--- src/helpers.js
+++ src/helpers.js
@@ -1,6 +1,6 @@
-const repeat = (str, times) => (new Array(times + 1)).join(str);
+export const repeat = (str, times) => (new Array(times + 1)).join(str);
 
-const pad = (num, maxLength) => repeat(`0`, maxLength - num.toString().length) + num;
+export const pad = (num, maxLength) => repeat(`0`, maxLength - num.toString().length) + num;
 
 export const formatTime = (time) => `${pad(time.getHours(), 2)}:${pad(time.getMinutes(), 2)}:${pad(time.getSeconds(), 2)}.${pad(time.getMilliseconds(), 3)}`;
 
