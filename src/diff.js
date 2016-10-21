9ba744f1c0e7c0b41a39fab96e2446737e84c792
Tim van der Horst <tim.vanderhorst@sportsmedglobal.com>
2016-10-21 12:02:10 +1000

    Log diff output directly instead of converting to string

--- src/diff.js
+++ src/diff.js
@@ -29,15 +29,15 @@ function render(diff) {
 
   switch (kind) {
     case `E`:
-      return `${path.join(`.`)} ${lhs} → ${rhs}`;
+      return [path.join(`.`), lhs, `→`, rhs];
     case `N`:
-      return `${path.join(`.`)} ${rhs}`;
+      return [path.join(`.`), rhs];
     case `D`:
-      return `${path.join(`.`)}`;
+      return [path.join(`.`)];
     case `A`:
       return [`${path.join(`.`)}[${index}]`, item];
     default:
-      return null;
+      return [];
   }
 }
 
@@ -59,7 +59,7 @@ export default function diffLogger(prevState, newState, logger, isCollapsed) {
       const { kind } = elem;
       const output = render(elem);
 
-      logger.log(`%c ${dictionary[kind].text}`, style(kind), output);
+      logger.log(`%c ${dictionary[kind].text}`, style(kind), ...output);
     });
   } else {
     logger.log(`—— no diff ——`);
