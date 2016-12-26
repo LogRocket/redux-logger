a4e1a98bede668fa1b73b7a16ba18341a922264c
Kareem Francis <francis.kareem+git@gmail.com>
2016-12-26 15:01:45 -0500

    Apply `actionTransformer` before persisting

--- src/persistence.js
+++ src/persistence.js
@@ -1,13 +1,18 @@
 import debounce from 'debounce';
 
-function persistBuffer(buffer, { persister }) {
-  persister(buffer)
+function persistBuffer(buffer, { persister, actionTransformer }) {
+  const transformedBuffer = buffer.map(({ action, ...logEntry }) => ({
+    ...logEntry,
+    action: actionTransformer(action),
+  }));
+
+  persister(transformedBuffer)
     .then(() => {
       // clear the buffer - explicit side-effect
       buffer.length = 0; // eslint-disable-line no-param-reassign
     })
     .catch(() => {
-      console.error(`failed to persist buffer`, buffer); // eslint-disable-line no-console
+      console.error(`failed to persist buffer`, transformedBuffer); // eslint-disable-line no-console
     });
 }
 
