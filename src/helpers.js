11bb06de4da817dd2efabedf2ff53a486035c268
Thomas Grainger <tom@procensus.com>
2017-05-31 19:43:30 +0100

    use object.assign pollfill
    to support ES5 environments eg IE11

--- src/helpers.js
+++ src/helpers.js
@@ -1,8 +1,12 @@
+import objectAssign from 'object.assign';
+
 export const repeat = (str, times) => (new Array(times + 1)).join(str);
 
 export const pad = (num, maxLength) => repeat('0', maxLength - num.toString().length) + num;
 
 export const formatTime = time => `${pad(time.getHours(), 2)}:${pad(time.getMinutes(), 2)}:${pad(time.getSeconds(), 2)}.${pad(time.getMilliseconds(), 3)}`;
+
+export const merge = (...args) => objectAssign({}, ...args);
 
 // Use performance API if it's available in order to get better precision
 export const timer =
