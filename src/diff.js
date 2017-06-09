0a1704bb9acc6e3a12a4e59cc118a29179a9117a
Tim van der Horst <tim.vanderhorst@sportsmedglobal.com>
2017-06-09 12:30:55 +1000

    feat: Extended diff option to allow for a custom differ function

--- src/diff.js
+++ src/diff.js
@@ -41,8 +41,8 @@ export function render(diff) {
   }
 }
 
-export default function diffLogger(prevState, newState, logger, isCollapsed) {
-  const diff = differ(prevState, newState);
+export default function diffLogger(prevState, newState, logger, isCollapsed, customDiffer) {
+  const diff = (customDiffer || differ)(prevState, newState);
 
   try {
     if (isCollapsed) {
