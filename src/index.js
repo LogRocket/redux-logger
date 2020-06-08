9e2023d8d05ead73880951778cc1110b6dfd609e
Wynnfan <Wynnfan1997@gmail.com>
2020-06-08 14:23:21 +0800

    Update index.js
    format console.error

--- src/index.js
+++ src/index.js
@@ -42,22 +42,22 @@ function createLogger(options = {}) {
   if (options.getState && options.dispatch) {
     // eslint-disable-next-line no-console
     console.error(`[redux-logger] redux-logger not installed. Make sure to pass logger instance as middleware:
-// Logger with default options
-import { logger } from 'redux-logger'
-const store = createStore(
-  reducer,
-  applyMiddleware(logger)
-)
-// Or you can create your own logger with custom options http://bit.ly/redux-logger-options
-import { createLogger } from 'redux-logger'
-const logger = createLogger({
-  // ...options
-});
-const store = createStore(
-  reducer,
-  applyMiddleware(logger)
-)
-`);
+      // Logger with default options
+      import { logger } from 'redux-logger'
+      const store = createStore(
+        reducer,
+        applyMiddleware(logger)
+      )
+      // Or you can create your own logger with custom options http://bit.ly/redux-logger-options
+      import { createLogger } from 'redux-logger'
+      const logger = createLogger({
+        // ...options
+      });
+      const store = createStore(
+        reducer,
+        applyMiddleware(logger)
+      )
+    `);
 
     return () => next => action => next(action);
   }
@@ -112,13 +112,13 @@ const defaultLogger = ({ dispatch, getState } = {}) => {
   }
   // eslint-disable-next-line no-console
   console.error(`
-[redux-logger v3] BREAKING CHANGE
-[redux-logger v3] Since 3.0.0 redux-logger exports by default logger with default settings.
-[redux-logger v3] Change
-[redux-logger v3] import createLogger from 'redux-logger'
-[redux-logger v3] to
-[redux-logger v3] import { createLogger } from 'redux-logger'
-`);
+    [redux-logger v3] BREAKING CHANGE
+    [redux-logger v3] Since 3.0.0 redux-logger exports by default logger with default settings.
+    [redux-logger v3] Change
+    [redux-logger v3] import createLogger from 'redux-logger'
+    [redux-logger v3] to
+    [redux-logger v3] import { createLogger } from 'redux-logger'
+  `);
 };
 
 export { defaults, createLogger, defaultLogger as logger };
